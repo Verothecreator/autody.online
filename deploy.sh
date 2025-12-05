23b0c0e6841928eec351e2caf0b908765827c8be
root <root@server1.autody.online>
2025-12-05 03:16:41 +0000

    Add auto-deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-cd /var/www/autody/autody.online || exit
-echo "🚀 Pulling latest code..."
+cd /var/www/autody/autody.online || exit 1
+
+echo "🚀 Deploy script started"
+
+# Pull latest code from GitHub
 git pull origin main
+
+# Install production deps
 npm install --production
+
+# Restart the live app
 pm2 restart autody.online
+
+echo "✅ Deploy finished"
+
